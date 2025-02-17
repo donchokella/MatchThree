28cc6bf310de9bd0e1c2cd4bfe8826ed69e8a60c
donchokella <ali.alparslan.94@outlook.com>
2025-02-17 20:14:55 +0300

    GameBoard and GamePieces(Statically) are created

---
+++ Assets/Scripts/GamePiece.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class GamePiece : MonoBehaviour
+{
+    public int xIndex;
+    public int yIndex;
+
+    public void SetCoord(int x, int y)
+    {
+        xIndex = x;
+        yIndex = y;
+    }
+
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public int xIndex;
+    public int yIndex;
+
+    Board m_board;
+
+
+    public void Init(int x, int y , Board board)
+    {
+        xIndex = x;
+        yIndex = y;
+        m_board = board;
+    }
+
+}
